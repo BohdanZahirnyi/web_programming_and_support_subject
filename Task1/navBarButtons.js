48deb46db85c81509544cc0384e6189df0e43c2f
Bohdan Zahirnuy <zahirnuy@ukr.net>
2019-02-18 00:41:02 +0200

    optimized backgr and navbar for mobile devices, backgr got some opacity,added js file and onclick func and files about.html, commonStyle.css

---
+++ Task1/navBarButtons.js
@@ -0,0 +1,8 @@
+function GoToMainP()
+{
+	window.location.href = 'file:///C:/Users/Bodya%20Zahirnuy/Documents/GitHub/web_programming_and_support_subject/Task1/main.html';
+}
+function GoToAboutP()
+{
+	window.location.href = 'file:///C:/Users/Bodya%20Zahirnuy/Documents/GitHub/web_programming_and_support_subject/Task1/about.html';
+}
