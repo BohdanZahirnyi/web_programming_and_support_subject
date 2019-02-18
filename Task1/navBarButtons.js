c5fe60fb8627565e61451f2cc4eb85b1e74d9650
Bohdan Zahirnuy <zahirnuy@ukr.net>
2019-02-18 14:43:12 +0200

    content added for main page

--- Task1/navBarButtons.js
+++ Task1/navBarButtons.js
@@ -6,3 +6,11 @@ function GoToAboutP()
 {
 	window.location.href = 'file:///C:/Users/Bodya%20Zahirnuy/Documents/GitHub/web_programming_and_support_subject/Task1/about.html';
 }
+function lightOn()
+{
+document.getElementById("brandImg").src="Light_bulb.png";
+}
+function lightOff()
+{
+document.getElementById("brandImg").src="Light_bubl2.png";
+}
