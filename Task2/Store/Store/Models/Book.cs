64786a57a35b123b71ec1455a4331f26da8a7003
Bohdan Zahirnuy <zahirnuy@ukr.net>
2019-05-04 02:03:59 +0300

    intial commit(created project, added db)

---
+++ Task2/Store/Store/Models/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Store.Models
+{
+    public class Book
+    {
+       
+        public int Id { get; set; }
+     
+        public string Name { get; set; }
+       
+        public string Author { get; set; }
+
+        public string Genre { get; set; }
+      
+        public int Price { get; set; }
+        
+    }
+}
---
+++ Task2/Store/Store/Models/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Store.Models
+{
+    public class Order
+    {
+            public int OrderId { get; set; }
+            public string Person { get; set; }
+            public string Address { get; set; }
+            public int BookId { get; set; }
+            public DateTime Date { get; set; }
+        
+    }
+}
